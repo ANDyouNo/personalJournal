2b4d6551213cc2513401b6560d0cd521b5256c3b
ANDyouNo <younofl312@gmail.com>
2024-06-16 14:43:07 +0300

    f

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/personalJournal",
+  base: "/personalJournal", // off for dev
 
   build: {
     sourcemap: true,
